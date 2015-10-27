d947ffe7440652ebaa5abef8a818dcab1d381c3b
Jared Gordon <jgordon@pivotal.io>
2015-10-27 16:13:45 -0500

    add pw

--- reset-vibs.sh
+++ reset-vibs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh ${PHOTON_INSTALLER_USER}@${PHOTON_INSTALLER_IP} -C "rm -rf /var/esxcloud/packages/esxcloud-0.1.1.promote*-5.5.0.vib" 
-scp ../photon/esxcloud-0.1.1.promote175-5.5.0.vib ${PHOTON_INSTALLER_USER}@${PHOTON_INSTALLER_IP}:/var/esxcloud/packages
+ssh ${PHOTON_INSTALLER_USER}:${PHOTON_INSTALLER_PASSWORD}@${PHOTON_INSTALLER_IP} -C "rm -rf /var/esxcloud/packages/esxcloud-0.1.1.promote*-5.5.0.vib" 
+scp ../photon/esxcloud-0.1.1.promote175-5.5.0.vib ${PHOTON_INSTALLER_USER}:${PHOTON_INSTALLER_PASSWORD}@${PHOTON_INSTALLER_IP}:/var/esxcloud/packages
