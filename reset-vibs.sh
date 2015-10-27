923e93ff3b0bc47d7f7bd4d4e9a254327b08de09
Jared Gordon <jgordon@pivotal.io>
2015-10-27 16:00:28 -0500

    Update reset-vibs.sh
    externalize env

--- reset-vibs.sh
+++ reset-vibs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh root@10.65.162.14 -C "rm -rf /var/esxcloud/packages/esxcloud-0.1.1.promote*-5.5.0.vib" 
-scp ../photon/esxcloud-0.1.1.promote175-5.5.0.vib root@10.65.162.14:/var/esxcloud/packages
+ssh ${PHOTON_INSTALLER_USER}@${PHOTON_INSTALLER_IP} -C "rm -rf /var/esxcloud/packages/esxcloud-0.1.1.promote*-5.5.0.vib" 
+scp ../photon/esxcloud-0.1.1.promote175-5.5.0.vib ${PHOTON_INSTALLER_USER}@${PHOTON_INSTALLER_IP}:/var/esxcloud/packages
