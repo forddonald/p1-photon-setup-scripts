268c3d7e1fb0ffe6a9ae1d84d4175d1ccbc840fb
forddonald <fdonald@pivotal.io>
2015-10-27 17:13:19 -0500

    Update set-photon-tenant.sh

--- set-photon-tenant.sh
+++ set-photon-tenant.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-photon -t http://${IP0}:${PHOTON_PORT} system deploy ../pcf-photon-manifests/photon/photon-env-01.yml
-sleep 600
-photon target set http://${PHOTON_CTRL_IP}:8080
-photon tenant create cf-test
+
+#photon -t http://10.65.162.14:9000 system deploy ../pcf-photon-manifests/photon/photon-env-01.yml
+#sleep 180
+photon target set ${PHOTON_TARGET}
+photon tenant create ${PHOTON_TARGET_NAME}
 photon tenant set cf-test
 photon resource-ticket create --name dev-ticket --limits "vm.memory 3600 GB, vm 10000 COUNT" -t cf-test
 photon project create --name dev-project --limits "vm.memory 3600 GB, vm 10000 COUNT" -r dev-ticket
