e31e8a1ff3ed607d9922608672040edd341e9e69
Jared Gordon <jgordon@pivotal.io>
2015-10-27 15:24:48 -0500

    Update deploy-photon-installer.sh
    externalize env specific values

--- deploy-photon-installer.sh
+++ deploy-photon-installer.sh
@@ -1,15 +1,15 @@
 ovftool --acceptAllEulas --noSSLVerify --skipManifestCheck \
 --X:injectOvfEnv --overwrite --powerOffTarget --powerOn \
 --diskMode=thin \
---net:"NAT"="Mgmt" \
---datastore=FS_PEZ_NFS_HAAS04 \
+--net:"NAT"=${VM_NETWORK} \
+--datastore=${DATASTORE} \
 --name=photon-installer \
---prop:ip0=10.65.162.14 \
---prop:netmask0=255.255.255.0 \
---prop:gateway=10.65.162.1 \
---prop:DNS=10.65.162.2 \
---prop:ntp_servers=10.65.162.2 \
+--prop:ip0=${IP0} \
+--prop:netmask0=${NETMASK0) \
+--prop:gateway=${GATEWAY} \
+--prop:DNS=${DNS} \
+--prop:ntp_servers=${NTP_SERVERS} \
 --prop:enable_syslog="true" \
---prop:syslog_endpoint=10.65.162.15 \
---prop:admin_password="d3v0ps!" /home/photon/photon/photon-ctrl-installer-152.ova \
-vi://root:d3v0ps!@10.65.162.104
+--prop:syslog_endpoint=${SYSLOG_ENDPOINT} \
+--prop:admin_password=${ADMIN_PASSWORD} ${PHOTON_CTRL_INSTALLER_OVA_PATH} \
+vi://${ESX_ROOT_USER}:${ESX_ROOT_PASSWORD}@${ESX_SERVER_IP}
